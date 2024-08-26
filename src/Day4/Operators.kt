4351b50fccff656ebea7cea3791a25b9a3d8272f
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2024-08-26 13:54:29 +0530

    day 5

--- src/Day4/Operators.kt
+++ src/Day4/Operators.kt
@@ -61,7 +61,6 @@ fun main(){
 
     // logical operators
 
-    println("")
 
 
 
---
+++ src/Day5/ControlFlow.kt
@@ -0,0 +1,101 @@
+package Day5
+import java.util.Scanner
+
+fun main(){
+//    val temp = 20
+//    if(temp>20){
+//        println("its a warm day")
+//    }
+//    else{
+//        println("its a cool day")
+//    }
+
+//    val marks = 67
+//    if (marks>=90){
+//        println("Grade - A")
+//    }
+//    else if(marks>=85){
+//        println("Grade - B")
+//    }
+//    else if(marks>=70){
+//        println("Grade - C")
+//    }
+//    else{
+//        println("Grade - D")
+//    }
+
+    //create a calculator with all the arthematic operators
+
+//    var num1:Int = 10
+//    var num2:Int = 20
+//    var op:Char = '+'
+//
+//    fun calc(num1:Int,num2:Int,op:Char):Unit{
+//        if(op=='+'){
+//            println("${num1+num2}")
+//        }
+//        else if(op=='-'){
+//            println("${num1-num2}")
+//        }
+//        else if(op=='*'){
+//            println("${num1*num2}")
+//        }
+//        else if(op=='/'){
+//            if(num2!=0){
+//                println("${num1/num2}")
+//            }
+//            else{
+//                println("cannot divide by 0")
+//            }
+//        }
+//        else if(op=='%'){
+//            println("${num1%num2}")
+//        }
+//    }
+//
+//    calc(num1,num2,op)
+
+    val reader =Scanner(System.`in`)
+//    print("Enter the age: ")
+//
+//
+//    var age:Int = reader.nextInt()
+//
+//    if(age<13){
+//        println("child")
+//    }
+//    else if( age>=13 && age<=19){
+//        println("teen")
+//    }
+//    else{
+//        println("adult")
+//    }
+
+
+    // using when
+
+//    print("Enter the day of Week: ")
+//    var dayOfWeek = reader.nextInt()
+//
+//    val dayname = when(dayOfWeek){
+//        1->"Monday"
+//        2->"Tuesday"
+//        3->"Wednesday"
+//        4->"Thursday"
+//        5->"Friday"
+//        6->"Saturday"
+//        7->"Sunday"
+//        else->"There r only 7 days in a week"
+//    }
+//    println("$dayname")
+
+//    val num = 6
+//    when{
+//        num%2==0->println("The number is even")
+//
+//        num%2!=0->println("The number is odd")
+//
+//        else->(println("Okay"))
+//    }
+
+}
