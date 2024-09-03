3690280d34853cc9265b4bcf3d469727c87abed6
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2024-09-03 19:11:08 +0530

    inetrfaces

---
+++ src/Day9_03Sept2024/Interface.kt
@@ -0,0 +1,29 @@
+package Day9_03Sept2024
+
+interface Shape{
+    fun area():Double
+}
+
+open class Rectangle(val a:Double,val b: Double):Shape{
+    override fun area():Double{
+        return a*b
+    }
+
+    open fun display(){
+        println("area of rectangle with dimensions $a * $b is ${area()}")
+    }
+}
+
+class Square(side:Double):Rectangle(side,side){
+    override fun display(){
+        println("area of square with side $a is ${area()}")
+    }
+}
+
+fun main(){
+    val myRectangle = Rectangle(4.0,5.0)
+    myRectangle.display()
+
+    val mySquare = Square(3.0)
+    mySquare.display()
+}
